2866d8caca04fc57885199db6eadf9d379535d18
aldemirmuhammed <aldemirrmuhammed2009@gmail.com>
2023-08-13 17:04:24 +0300

    Adding delete photos method

--- UdemyMicroservices/Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
+++ UdemyMicroservices/Services/PhotoStock/FreeCourse.Services.PhotoStock/Controllers/PhotosController.cs
@@ -19,7 +19,7 @@ namespace FreeCourse.Services.PhotoStock.Controllers
         {
             if (photo != null && photo.Length > 0)
             {
-                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photo", photo.FileName);
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
                 using (var stream = new FileStream(path, FileMode.Create))
                 {
                     await photo.CopyToAsync(stream, cancellationToken);
@@ -30,6 +30,20 @@ namespace FreeCourse.Services.PhotoStock.Controllers
                 return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
             }
             return CreateActionResultInstance(Response<PhotoDto>.Fail("photo is empty",400));
+        }
+
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+
+            if(!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("Photo not found", 404));
+            }
+
+            System.IO.File.Delete(path);
+
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
         }
     }
 }
