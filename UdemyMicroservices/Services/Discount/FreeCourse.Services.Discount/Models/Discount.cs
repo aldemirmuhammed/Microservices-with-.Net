4cf2c06b0a81a8d1035e3a6b4fc3d36e9e1c674b
aldemirmuhammed <aldemirrmuhammed2009@gmail.com>
2023-08-20 18:24:22 +0300

    Added discount microservice

---
+++ UdemyMicroservices/Services/Discount/FreeCourse.Services.Discount/Models/Discount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FreeCourse.Services.Discount.Models
+{
+    // Mapping model to database table
+    [Dapper.Contrib.Extensions.Table("discount")]
+    public class Discount
+    {
+
+        public int Id{ get; set; }
+        public string UserId{ get; set; }
+        public int Rate{ get; set; }
+        public string Code{ get; set; }
+        public DateTime CreatedTime{ get; set; }
+    }
+}
