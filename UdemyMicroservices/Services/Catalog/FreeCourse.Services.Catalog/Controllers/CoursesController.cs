71174fc8fcab5b55d0c609782f089bb5bb760063
aldemirmuhammed <aldemirrmuhammed2009@gmail.com>
2023-10-12 15:43:18 +0300

    added docker compose

--- UdemyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/Controllers/CoursesController.cs
+++ UdemyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/Controllers/CoursesController.cs
@@ -38,7 +38,6 @@ namespace FreeCourse.Services.Catalog.Controllers
 
         [HttpGet]
         [Route("/api/[controller]/GetAllByUserId/{userId}")]
-
         public async Task<IActionResult> GetAllByUserId(string userId)
         {
             var response = await _courseService.GetAllByUserIdAsync(userId);
